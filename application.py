41e70f36a44fcfa2fc5181c0530a1d3aa370cd69
omjkbp-cpu <omjkbp@gmail.com>
2026-06-25 12:49:47 +0530

    Add files via upload
    first

---
+++ application.py
@@ -0,0 +1,10 @@
+import reflex as rx
+
+config = rx.Config(
+    app_name="link_bio",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://moure.dev"
+    ],
+    show_built_with_reflex=False
+)
